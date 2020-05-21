1029a860ebe81af7f780fce3ebeb32ba4551512e
Brad Sease <bradsease@gmail.com>
2020-05-21 00:49:01 +0000

    Prepare for first package version

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 import setuptools
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+version = os.environ.get("CI_COMMIT_TAG")
+if version is None:
+    raise ValueError("Version not found.")
+
 setuptools.setup(
     name="oem",
-    version="0.1.0",
+    version=version,
     author="Brad Sease",
     author_email="bradsease@gmail.com",
     description="Python Orbital Ephemeris Message (OEM) tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bradsease/oem",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
