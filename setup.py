6caf19a91caa110bcae1d9bc0470be3225a695c7
Brad Sease <bradsease@gmail.com>
2021-05-16 00:32:38 +0000

    Improve package metadata.

--- setup.py
+++ setup.py
@@ -19,9 +19,22 @@ setuptools.setup(
     url="https://github.com/bradsease/oem",
     packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Astronomy"
     ],
-    python_requires='>=3.0',
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'astropy',
+        'lxml'
+    ]
 )
