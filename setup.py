6ea6971b04d5f04999b3ad8cb5b5522d1429fb93
Tommaso <Tommaso.pino@me.com>
2021-12-06 08:37:16 +0100

    Update setup.py
    reverting to the original setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = 'v0.3.1'#os.environ.get("CI_COMMIT_TAG")[1:]
+version = os.environ.get("CI_COMMIT_TAG")[1:]
 if version is None:
     raise ValueError("Version not found.")
 
