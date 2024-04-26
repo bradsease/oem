1b2ab64c894e0638e1d7b7268158bc64cdeb6961
Kyle Cochran <kyle.cochran@loftorbital.com>
2024-04-26 04:44:05 +0000

    fix: convert to datetime first in format_epoch to retain millisecond precision

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 import pytest
+from astropy.time import Time
 
 from oem import tools
 
@@ -8,3 +11,9 @@ def test_parse_integer():
     tools.parse_integer(1.0, None)
     with pytest.raises(ValueError):
         tools.parse_integer(1.1, None)
+
+def test_format_epoch():
+    assert tools.format_epoch(datetime.fromisoformat("2024-02-08T19:46:03.597928")) == "2024-02-08T19:46:03.597928"
+    assert tools.format_epoch(Time("2024-02-08T19:46:03.597928")) == "2024-02-08T19:46:03.597928"
+    with pytest.raises(ValueError):
+        tools.format_epoch("2024-02-08T19:46:03.597928Z")
